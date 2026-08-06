e974df6ad738daa8ebca81cb75cfa3800a81e2ac
yswann <mainichiganbal@gmail.com>
2026-08-06 10:11:13 +0900

    add extlibs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(extlibs.plugins.kotlin.android)
+    alias(extlibs.plugins.kotlin.compose)
 }
 
 android {
