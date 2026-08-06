11a806726de5dad29748bd18eeb5389ae6f87641
yswann <mainichiganbal@gmail.com>
2026-08-06 16:02:40 +0900

    fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,10 @@
 pluginManagement {
     repositories {
         google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
             }
         }
         mavenCentral()
@@ -14,16 +14,19 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
     }
 
     versionCatalogs {
         create("extlibs") {
             from(files("extlibs/gradle/libs.versions.toml"))
-        }
-        create("libs") {
-            from(files("libs/gradle/libs.versions.toml"))
         }
     }
 }
